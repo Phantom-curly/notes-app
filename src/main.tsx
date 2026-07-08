6525d139bccdbab3047c7517eab90e275d4a426b
Phantom-curly <dilmurod111804@gmail.com>
2026-07-08 21:12:00 +0900

    Fix JSX namespace with react-jsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+console.log('🚀 App is starting!');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
